32869dd8d7590479a52ae555195941882d887b02
maryam <maryam.askari@irori.se>
2022-07-20 11:09:55 +0200

    Initial commit

---
+++ src/main/java/com/example/iroribank/IroriBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.iroribank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroriBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IroriBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iroribank/IroriBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iroribank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IroriBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
