fd41b2bdf7ad2e29ec7411187ee59a323c382eb4
maryam <maryam.askari@irori.se>
2022-07-20 12:08:38 +0200

    Added repositories for Customer and Account.

---
+++ src/main/java/com/example/iroribank/repo/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.example.iroribank.repo;
+
+import com.example.iroribank.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+}
---
+++ src/main/java/com/example/iroribank/repo/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.iroribank.repo;
+
+import com.example.iroribank.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
